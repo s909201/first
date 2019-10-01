a4d62005cbe62cd4916e2d86c39ec80cb2b87d2c
s909201 <s909201@gmail.com>
2019-10-01 17:43:29 +0800

    update halt.py, the BTN GPIO is changed from GPIO 3 to 21

--- halt.py
+++ halt.py
@@ -5,11 +5,11 @@ import RPi.GPIO as GPIO
 import subprocess
 
 # Define GPIO Pin Number
-gpioPin=3
+gpioPin=21
 
 # Define GPIO Function
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(gpioPin, GPIO.IN)
+GPIO.setup(gpioPin, GPIO.IN, GPIO.PUD_UP)
 GPIO.wait_for_edge(gpioPin, GPIO.FALLING)
 
 # Command
